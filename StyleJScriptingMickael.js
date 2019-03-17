80c2818c2efedfa6112b6384324320c5b55f94de
dinak77 <45309233+dinak77@users.noreply.github.com>
2019-03-17 17:55:26 +0100

    Update StyleJScriptingMickael.js

--- StyleJScriptingMickael.js
+++ StyleJScriptingMickael.js
@@ -1,14 +1,16 @@
-let question = prompt("Hey mon ami ! Tu aimes çà les patates ?", " oui ou non");
+let question = prompt("Hey mon ami ! Tu aimes çà les patates ?", " oui ou non");{
 if (question == 'oui'){
-    let tas = '#';
-    let i = 0;
-    for (i = 0; i < 7; i++){
-    console.log(tas);
-    tas = tas +'#';
+    	let tas = '#';
+    	let i = 0;
+    	for (i = 0; i < 7; i++){
+		console.log(tas);
+    		tas = tas +'#';
     }
-    }
-    else if (question == 'non')
+}
+else if (question == 'non'){
     alert("Ne me dis pas que tu préfères les champignon ! :(");
-    else
+	}
+else{
     alert("Je vous sens comme tiraillé !");
-    
+	}
+}
